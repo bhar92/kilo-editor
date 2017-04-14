ce09e69f7d175fcd46d7b219b0e6955bdc868ed4
Bhargava Venkatesh <bhar92@gmail.com>
2017-04-15 01:41:21 +0530

    Add comments

--- kilo.c
+++ kilo.c
@@ -13,11 +13,15 @@ void disableRawMode(void)
 
 void enableRawMode(void)
 {
+	/* Obtain the terminal settings and store it in a struct */
 	tcgetattr(STDIN_FILENO, &orig_termios);
+
+	/* Disable raw mode on exit */
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
 
+	/* Disable echo and turn offcanonical mode */
 	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -29,6 +33,11 @@ int main(void)
 
 	char c;
 
+	/*
+	 * 1. Read 1 byte into variable c and keep doing it. And press q
+	 * to quit.
+	 * 2. Print the character and it's ASCII value.
+	 */
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
 		if (iscntrl(c))
 			printf("%d\n", c);
